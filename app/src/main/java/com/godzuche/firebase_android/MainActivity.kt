3165ff83283936aa4ca1c06642cc29bf4f7ff5bb
Administrator <godxuche@gmail.com>
2021-11-07 14:29:03 +0100

    successfully set up the project and added firebase to it...

--- app/src/main/java/com/godzuche/firebase_android/MainActivity.kt
+++ app/src/main/java/com/godzuche/firebase_android/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.godzuche.firebase_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
